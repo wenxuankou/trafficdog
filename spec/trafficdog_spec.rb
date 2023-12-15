18e816b54d735807ef5e677873ad0440d08415de
Eidson Kou <wenxuankou1993@outlook.com>
2023-12-15 11:28:15 +0800

    init

--- spec/trafficdog_spec.rb
+++ spec/trafficdog_spec.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 RSpec.describe Trafficdog do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Trafficdog::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'does something useful' do
+    expect(false).to eq(false)
   end
 end
